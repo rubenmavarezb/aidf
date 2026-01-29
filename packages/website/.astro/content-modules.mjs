c6c919c6d5085cbc1a25c2b32d8008685443297d
Ruben <ruben.mavarez@pivotree.com>
2026-01-29 20:31:42 -0300

    aidf: Que los templates generados por `aidf init` expliq...

--- packages/website/.astro/content-modules.mjs
+++ packages/website/.astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/fr/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/es/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/fr/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/pt/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
